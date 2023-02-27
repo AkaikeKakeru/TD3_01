fc6aa86ee572043b84bc8c38ed36d949d8e727eb
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-27 10:25:40 +0900

    衝突時にコールバック関数を呼び出す

--- TD3_01/Application/Function/Collider/BaseCollider.h
+++ TD3_01/Application/Function/Collider/BaseCollider.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CollisionTypes.h"
+#include "CollisionInfo.h"
 #include "Object3d.h"
 
 class BaseCollider {
@@ -18,6 +19,11 @@ public: //アクセッサ
 	//形状タイプ取得
 	inline CollisionShapeType GetShapeType() {
 		return shapeType_; }
+
+	//衝突時コールバック関数
+	inline void OnCollision(const CollisionInfo& info) {
+		object_->OnCollision(info);
+	}
 
 protected: //メンバ変数
 	Object3d* object_ = nullptr;
