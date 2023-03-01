9120a64a05c554d09f16470669e5fe9f5a2c235a
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-03-01 14:37:56 +0900

    衝突マネージャーでレイキャストを定義

--- TD3_01/Application/Function/Collider/CollisionManager.cpp
+++ TD3_01/Application/Function/Collider/CollisionManager.cpp
@@ -20,22 +20,71 @@ void CollisionManager::CheckAllCollisions() {
 			BaseCollider* colA = *itA;
 			BaseCollider* colB = *itB;
 
-			//ともに球
-			CheckSphere2Sphere(colA,colB);
+			if (colA->GetShapeType() == COLLISIONSHAPE_SPHERE
+				&& colB->GetShapeType() == COLLISIONSHAPE_SPHERE) {
+				//ともに球
+				CheckSphere2Sphere(colA, colB);
+			}
 		}
 	}
 }
 
 void CollisionManager::CheckSphere2Sphere(BaseCollider* colA, BaseCollider* colB) {
-	if (colA->GetShapeType() == COLLISIONSHAPE_SPHERE
-		&& colB->GetShapeType() == COLLISIONSHAPE_SPHERE) {
-		Sphere* SphereA = dynamic_cast<Sphere*>(colA);
-		Sphere* SphereB = dynamic_cast<Sphere*>(colB);
-		Vector3 inter;
+	Sphere* SphereA = dynamic_cast<Sphere*>(colA);
+	Sphere* SphereB = dynamic_cast<Sphere*>(colB);
+	Vector3 inter;
 
-		if (Collision::CheckSphere2Sphere(*SphereA, *SphereB, &inter)) {
-			colA->OnCollision(CollisionInfo(colB->GetObject3d(), colB, inter));
-			colB->OnCollision(CollisionInfo(colA->GetObject3d(), colA, inter));
+	if (Collision::CheckSphere2Sphere(*SphereA, *SphereB, &inter)) {
+		colA->OnCollision(CollisionInfo(colB->GetObject3d(), colB, inter));
+		colB->OnCollision(CollisionInfo(colA->GetObject3d(), colA, inter));
+	}
+}
+
+bool CollisionManager::Raycast(const Ray& ray, RaycastHit* hitInfo,
+	float maxDistance) {
+	bool result = false;
+
+	//走査用イテレータ
+	std::forward_list<BaseCollider*>::iterator it;
+	//今までで最も近いコライダーを記録するためのイテレータ
+	std::forward_list<BaseCollider*>::iterator it_hit;
+	//今までで最も近いコライダーの距離を記録する変数
+	float distance = maxDistance;
+	//今までで最も近いコライダーとの交点を記録するための変数
+	Vector3 inter;
+
+	//全てのコライダーと総当たりチェック
+	it = colliders_.begin();
+	for (; it != colliders_.end(); ++it) {
+		BaseCollider* colA = *it;
+
+		//球の場合
+		if (colA->GetShapeType() == COLLISIONSHAPE_SPHERE) {
+			Sphere* sphere = dynamic_cast<Sphere*>(colA);
+			float tempDistance;
+			Vector3 tempInter;
+
+			//当たらなければ除外
+			if (!Collision::CheckRay2Sphere(ray, *sphere, &tempDistance, &tempInter))continue;
+
+			//距離が最小でなければ除外
+			if (tempDistance >= distance)continue;
+
+			//今までで最も近いので記録を取る
+			result = true;
+			distance = tempDistance;
+			inter = tempInter;
+			it_hit = it;
 		}
 	}
+
+	//最終的に何かに当たっていたら結果を書き込む
+	if (result && hitInfo) {
+		hitInfo->distance_ = distance;
+		hitInfo->inter_ = inter;
+		hitInfo->collider_ = *it_hit;
+		hitInfo->object_ = hitInfo->collider_->GetObject3d();
+	}
+
+	return result;
 }
