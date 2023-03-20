3b2563fd062bf1e4d288ad066a4f08ea2200848f
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-03-20 12:08:29 +0900

    ゴール用衝突属性(現状不要かも)

--- TD3_01/Application/Function/Collider/CollisionAttribute.h
+++ TD3_01/Application/Function/Collider/CollisionAttribute.h
@@ -4,3 +4,4 @@
 const unsigned short COLLISION_ATTR_LANDSHAPE = 0b1 << 0;//地形
 const unsigned short COLLISION_ATTR_PLAYER = 0b1 << 1;//プレイヤー陣営
 const unsigned short COLLISION_ATTR_RAY = 0b1 << 2;//レイ属性
+const unsigned short COLLISION_ATTR_GOAL = 0b1 << 3;//ゴール属性
