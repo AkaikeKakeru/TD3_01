c283f03619dae20572aeaacd6569e055be68ed83
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-03-26 18:54:06 +0900

    シェーダーファイルと、パーティクル画像を追加

--- TD3_01/Resource/shader/ParticleVS.hlsl
+++ TD3_01/Resource/shader/ParticleVS.hlsl
@@ -1,9 +1,10 @@
 #include "Particle.hlsli"
 
-VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD) {
+VSOutput main(float4 pos : POSITION,float scale : SCALE,float4 color : COLOR){
 	VSOutput output; // ピクセルシェーダーに渡す値
-	output.svpos = mul(mat, pos);
-	output.normal = normal;
-	output.uv = uv;
+	output.pos = pos;
+	output.scale = scale;
+	output.color = color;
+
 	return output;
 }
